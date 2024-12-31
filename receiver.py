43de747cea65459c134dbc30dcc0a2e16ca5d261
jawakarsri <jawaharsri2003@gmail.com>
2024-12-31 17:34:07 +0530

    Refactor receiver initialization in receiver.py

--- receiver.py
+++ receiver.py
@@ -1,8 +1,7 @@
 from vidstream import StreamingServer
 import threading
 
-receiver = StreamingServer('192.168.56.1', 9999)
-
+receiver = StreamingServer('192.168.56.1', 9999) 
 t= threading.Thread(target=receiver.start_server)
 t.start()
 
